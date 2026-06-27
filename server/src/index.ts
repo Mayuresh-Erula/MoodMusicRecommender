5a3525becd5bab98fa715c6578e984a8a36406af
Mayuresh-Erula <mayuresherula@gmail.com>
2026-06-27 20:03:42 +0530

    Temporarily allow all origins

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,37 @@
-import express from "express";
+// import express from "express";
+// import cors from "cors";
+// import { getSongsByMood } from "./controllers/songController";
+
+// const app = express();
+
+// const PORT = process.env.PORT || 3000;
+
+// // app.use(cors({
+// //   origin: [
+// //     "http://localhost:5173","https://mood-music-recommender-eight.vercel.app/"
+// //   ],
+// //   credentials: true
+// // }));
+// app.use(cors());
+// app.use(express.json());
+
+// app.get("/api/songs", getSongsByMood);
+
+// app.get("/health", (req, res) => {
+//   res.send("API is Running 🎵");
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 import cors from "cors";
+import express  from "express";
 import { getSongsByMood } from "./controllers/songController";
+const app = express()
 
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-// app.use(cors({
-//   origin: [
-//     "http://localhost:5173","https://mood-music-recommender-eight.vercel.app/"
-//   ],
-//   credentials: true
-// }));
 app.use(cors());
+
 app.use(express.json());
 
 app.get("/api/songs", getSongsByMood);
-
-app.get("/health", (req, res) => {
-  res.send("API is Running 🎵");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
